5fc1de057eddfd8752917c28bb4f64c8ca4c78aa
oren <oren@staff.42.fr>
2017-09-21 17:19:07 +0200

    [test] overide controller

--- lib/preview_pdf.rb
+++ lib/preview_pdf.rb
@@ -1,24 +1,23 @@
-module PreviewPdf
+require "cancan"
+
+module PreviewPdfAddOn
   extend ActiveSupport::Concern
 
-  module ClassMethods 
+  module ClassMethods
+    def preview_pdf opts = {}
 
-    def preview
-      name_method = resource.name_method
-      @pdf = resource.send(name_method, params)
-      render 'preview.pdf' if params[:preview]
+      define_method :preview do
+      
+        name_method = opts[:method] || resource.name_method
+        name_view = opts[:view] || 'preview.pdf'
+        @pdf = resource.send(name_method, params)
+        render name_view if params[:preview]
+
+      end
     end
-
   end
 end
 
-
-ActiveSupport.on_load(:action_controller) do
-  if self == ActionController::Base
-    def self.preview_pdf
-      PreviewPdf::Base.preview_pdf(self)
-      # initialize_resources_class_accessors!
-      # create_resources_url_helpers!
-    end
-  end
+class ActionController::Base
+  include PreviewPdfAddOn
 end
